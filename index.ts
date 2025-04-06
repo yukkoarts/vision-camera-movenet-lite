21585d69fb004e922ccc02a56edf986eef8ea5a9
yukkoarts <125702347+yukkoarts@users.noreply.github.com>
2025-04-06 16:50:39 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+//index.ts
+import { Frame } from 'react-native-vision-camera';
+import { Worklets } from 'react-native-worklets-core';
+
+/**
+ * Run on the native side and return a pose.
+ * You must install this plugin natively for this to work.
+ */
+export const scanPoses = Worklets.createRunOnUI((frame: Frame) => {
+  'worklet';
+  // This function is replaced by native implementation
+  return {};
+});
